7ca498232577d09f56a8fe7b42a01d991e464a84
Mijo Grabovac <73522966+Sky0711@users.noreply.github.com>
2023-07-31 15:59:31 +0200

    Cleaned up tests

--- test/BasicTests.ts
+++ test/BasicTests.ts
@@ -4,124 +4,102 @@ import { Contract, Signer } from "ethers";
 import { Deployer } from "../scripts/deploy";
 
 describe("EarthquakeInsurance", function () {
-  let deployer: Deployer;
+  let earthquakeInsurance: Contract;
+  let protectionNFT: Contract;
   let owner: Signer;
   let addr1: Signer;
   let addr2: Signer;
   let zeroAddress: string;
 
-  beforeEach(async function () {
-    deployer = new Deployer();
-    await deployer.deployContracts();
+  // Constants used across multiple test cases
+  const zone = "Zone1";
+  const value = ethers.utils.parseEther("1");
 
+  beforeEach("Deploying contracts and setting up signers", async function () {
+    const deployer = new Deployer();
+    await deployer.deployContracts();
+  
+    // Destructuring assignment from deployer
+    ({ earthquakeInsurance, protectionNFT } = deployer);
+  
     [owner, addr1, addr2] = await ethers.getSigners();
     zeroAddress = "0x0000000000000000000000000000000000000000";
   });
 
-  it("1. Should correctly mint the first token with ID 1", async function() {
-    const buyer = await addr1.getAddress();
-    const zone = "Zone1";
-    const value = ethers.utils.parseEther("1");
-
-    // Buy a policy
-    await deployer.earthquakeInsurance.connect(addr1).buyPolicy(zone, { value });
-
-    // Assume the first token minted has tokenId 1
-    const tokenId = 1;
-    const tokenOwner = await deployer.protectionNFT.ownerOf(tokenId);
-
-    expect(tokenOwner).to.equal(await addr1.getAddress());
-  });
-
-  it("2. Should correctly mint the second token with ID 2", async function() {
+  it("1. Should correctly mint two tokens with ID 1 and 2 respectively", async function() {
     const buyer1 = await addr1.getAddress();
     const buyer2 = await addr2.getAddress();
-    const zone = "Zone1";
-    const value = ethers.utils.parseEther("1");
-
-    // Buy policies for two different buyers
-    await deployer.earthquakeInsurance.connect(addr1).buyPolicy(zone, { value });
-    await deployer.earthquakeInsurance.connect(addr2).buyPolicy(zone, { value });
-
+  
+    // Buy a policy for buyer1
+    await earthquakeInsurance.connect(addr1).buyPolicy(zone, { value });
+    // Assume the first token minted has tokenId 1
+    const tokenId1 = 1;
+    const tokenOwner1 = await protectionNFT.ownerOf(tokenId1);
+    expect(tokenOwner1).to.equal(buyer1);
+  
+    // Buy a policy for buyer2
+    await earthquakeInsurance.connect(addr2).buyPolicy(zone, { value });
     // Assume the second token minted has tokenId 2
-    const tokenId = 2;
-    const tokenOwner = await deployer.protectionNFT.ownerOf(tokenId);
-
-    expect(tokenOwner).to.equal(await addr2.getAddress());
+    const tokenId2 = 2;
+    const tokenOwner2 = await protectionNFT.ownerOf(tokenId2);
+    expect(tokenOwner2).to.equal(buyer2);
   });
 
-  it("3. Should fail when trying to buy a policy with 0 value", async function() {
-    const zone = "Zone1";
-    await expect(deployer.earthquakeInsurance.connect(addr1).buyPolicy(zone, { value: 0 }))
+  it("2. Should fail when trying to buy a policy with 0 value", async function() {
+    await expect(earthquakeInsurance.connect(addr1).buyPolicy(zone, { value: 0 }))
       .to.be.revertedWith("Policy cost must be greater than 0");
   });
 
-  it("4. Should fail when trying to buy a policy in an affected zone", async function() {
-    const zone = "Zone1";
-    const value = ethers.utils.parseEther("1");
-
-    await deployer.earthquakeInsurance.connect(owner).setAffectedZone(zone);
-    await expect(deployer.earthquakeInsurance.connect(addr1).buyPolicy(zone, { value }))
+  it("3. Should fail when trying to buy a policy in an affected zone", async function() {
+    await earthquakeInsurance.connect(owner).setAffectedZone(zone);
+    await expect(earthquakeInsurance.connect(addr1).buyPolicy(zone, { value }))
       .to.be.revertedWith("This zone is currently affected by an earthquake");
   });
 
-  it("5. Should correctly mark a zone as affected", async function() {
-    const zone = "Zone1";
-
-    await deployer.earthquakeInsurance.connect(owner).setAffectedZone(zone);
-    const isZoneAffected = await deployer.earthquakeInsurance.affectedZones(zone);
+  it("4. Should correctly mark a zone as affected", async function() {
+    await earthquakeInsurance.connect(owner).setAffectedZone(zone);
+    const isZoneAffected = await earthquakeInsurance.affectedZones(zone);
 
     expect(isZoneAffected).to.be.true;
   });
 
-  it("6. Should fail when a non-owner tries to mark a zone as affected", async function() {
-    const zone = "Zone1";
-
-    await expect(deployer.earthquakeInsurance.connect(addr1).setAffectedZone(zone))
+  it("5. Should fail when a non-owner tries to mark a zone as affected", async function() {
+    await expect(earthquakeInsurance.connect(addr1).setAffectedZone(zone))
       .to.be.revertedWith("Ownable: caller is not the owner");
   });
 
-  it("7. Should correctly claim a payout", async function() {
-    const zone = "Zone1";
-    const value = ethers.utils.parseEther("1");
-
+  it("6. Should correctly claim a payout", async function() {
     // Buy a policy and assume the first token minted has tokenId 1
-    await deployer.earthquakeInsurance.connect(addr1).buyPolicy(zone, { value });
+    await earthquakeInsurance.connect(addr1).buyPolicy(zone, { value });
     const tokenId = 1;
 
     // Mark zone as affected and claim payout
-    await deployer.earthquakeInsurance.connect(owner).setAffectedZone(zone);
-    await deployer.earthquakeInsurance.connect(addr1).claimPayout(tokenId);
+    await earthquakeInsurance.connect(owner).setAffectedZone(zone);
+    await earthquakeInsurance.connect(addr1).claimPayout(tokenId);
 
     // Token should no longer exist
-    await expect(deployer.protectionNFT.ownerOf(tokenId))
+    await expect(protectionNFT.ownerOf(tokenId))
       .to.be.revertedWith("ERC721: invalid token ID");
   });
 
-  it("8. Should fail when a non-policy holder tries to claim a payout", async function() {
-    const zone = "Zone1";
-    const value = ethers.utils.parseEther("1");
-
+  it("7. Should fail when a non-policy holder tries to claim a payout", async function() {
     // Buy a policy and assume the first token minted has tokenId 1
-    await deployer.earthquakeInsurance.connect(addr1).buyPolicy(zone, { value });
+    await earthquakeInsurance.connect(addr1).buyPolicy(zone, { value });
     const tokenId = 1;
 
     // Mark zone as affected and try to claim payout with a different address
-    await deployer.earthquakeInsurance.connect(owner).setAffectedZone(zone);
-    await expect(deployer.earthquakeInsurance.connect(addr2).claimPayout(tokenId))
+    await earthquakeInsurance.connect(owner).setAffectedZone(zone);
+    await expect(earthquakeInsurance.connect(addr2).claimPayout(tokenId))
       .to.be.revertedWith("Not the holder of this policy");
   });
 
-  it("9. Should fail when trying to claim a payout in a non-affected zone", async function() {
-    const zone = "Zone1";
-    const value = ethers.utils.parseEther("1");
-
+  it("8. Should fail when trying to claim a payout in a non-affected zone", async function() {
     // Buy a policy and assume the first token minted has tokenId 1
-    await deployer.earthquakeInsurance.connect(addr1).buyPolicy(zone, { value });
+    await earthquakeInsurance.connect(addr1).buyPolicy(zone, { value });
     const tokenId = 1;
 
     // Try to claim payout without marking the zone as affected
-    await expect(deployer.earthquakeInsurance.connect(addr1).claimPayout(tokenId))
+    await expect(earthquakeInsurance.connect(addr1).claimPayout(tokenId))
       .to.be.revertedWith("Not within the affected area");
   });
 
